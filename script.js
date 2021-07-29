a30dd7ff48b78b9c537c68cfd19344f05cf7ac6b
kelley-mcd <kmcdonnell8@gmail.com>
2021-07-29 16:17:30 -0400

    commit

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 const WeathAPIkey = "38d375f0ae73a2b7f0a1570df2ff4387";
 
-const searchCity = document.querySelector('#search-input');
+const searchCity = document.querySelector('#search-form');
+
+
+function searchFormSubmit(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector('#search-input').value;
+  let formatInput = document.querySelector('#format-input').value;
+
+  if (!searchInput) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  let querryString = './search-results.htmlq=' + searchInput + "$format=" + formatInput;
+
+  location.assign(querryString);
+}
+
+searchCity.addEventListener('submit', searchFormSubmit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const fetchButton = document.querySelector('#fetch-button');
 
